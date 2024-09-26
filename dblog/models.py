7e8f1bdbebb82f20d9f119f11e9dcc52bc269538
Matthew Aitken <matt@itken.net>
2024-09-26 10:52:07 +0000

    Add html templates and connect index.html

--- dblog/models.py
+++ dblog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
         return self.title
     
     def number_of_likes(self):
-        return self.likes(count)
+        return self.likes.count()
 
 
 class Comment(models.Model):
