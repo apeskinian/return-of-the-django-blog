e22368c6ef9476f8b29289f48b8b3888c5187ceb
Matthew Aitken <matt@itken.net>
2024-09-26 08:22:45 +0000

    Set debug True for deployment test

--- codeblog/settings.py
+++ codeblog/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-apeskinian-returnofthed-qfpi7ffcrah.ws.codeinstitute-ide.net','.herokuapp.com']
 
